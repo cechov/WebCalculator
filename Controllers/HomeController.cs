abe14ccc35d69f95a8550b502890f2022fd43b24
cechov <cecilie.hovring@hotmail.com>
2018-01-12 16:22:21 +0100

    WebCalculator

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(double firstNumber, double secondNumber, string operation)
+        {
+            double total = 0.0;
+
+            switch (operation)
+            {
+                case "+":
+                    total = firstNumber + secondNumber;
+                    ViewBag.total = total;
+                    break;
+                case "-":
+                    total = firstNumber - secondNumber;
+                    ViewBag.total = total;
+                    break;
+                case "*":
+                    total = firstNumber * secondNumber;
+                    ViewBag.total = total;
+                    break;
+                case "/":
+                    total = firstNumber / secondNumber;
+                    ViewBag.total = total;
+                    break;
+                default:
+                    total = 0;
+                    break;
+            }
+
+            return View();
+        }
+    }
+}
